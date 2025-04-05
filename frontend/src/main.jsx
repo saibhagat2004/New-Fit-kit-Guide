f8e6ef6568c0ca5cadb51f784cb85cac69953ae2
Sai Bhagat <saibhagat2004@gmail.com>
2025-04-05 21:09:26 +0530

    googleAuth Key hide

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-
+const googleAuthKey = import.meta.env.VITE_GOOGLE_AUTH_KEY;
+console.log("Google Auth Key:", googleAuthKey);
 const queryClient= new QueryClient({
   defaultOptions:{
     refetchOnWindowFocus: false,//React Query will refetch data when the user returns to the tab/window where your application is running. Setting this to false prevents this behavior, which can be useful if you want to minimize unnecessary network requests or if your data does not change frequently.
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <GoogleOAuthProvider clientId="192149123402-dde505u014sh32bi8rblk1feitetq9td.apps.googleusercontent.com">
+        <GoogleOAuthProvider  clientId={googleAuthKey}>
            <App />  
         </GoogleOAuthProvider>       
       </QueryClientProvider>
