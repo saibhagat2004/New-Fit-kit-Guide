ad68331837f741a01c2bd8815ca6d34e99687599
Sai Pravin Bhagat <126854607+saibhagat2004@users.noreply.github.com>
2025-04-10 19:34:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const googleAuthKey = import.meta.env.VITE_GOOGLE_AUTH_KEY;
-console.log("Google Auth Key:", googleAuthKey);
 const queryClient= new QueryClient({
   defaultOptions:{
     refetchOnWindowFocus: false,//React Query will refetch data when the user returns to the tab/window where your application is running. Setting this to false prevents this behavior, which can be useful if you want to minimize unnecessary network requests or if your data does not change frequently.
